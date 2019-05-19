da1603bba360fbd2180aeb65003a3521c753c021
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2019-05-19 04:49:43 +0300

    Create cart demo page

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,83 @@
 <script>
-	export let name;
+  import Product from "./Product.svelte";
+  import Button from "./Button.svelte";
+  import Cart from "./Cart.svelte";
+
+  let title = "";
+  let price = 0;
+  let description = "";
+
+  let products = [];
+  let cartItems = [];
+
+  function setTitle(event) {
+    title = event.target.value;
+  }
+
+  function createProduct() {
+    const newProduct = {
+      title: title,
+      price: price,
+      description: description
+    };
+    products = products.concat(newProduct);
+  }
+
+  function addToCart(event) {
+    const selectedTitle = event.detail;
+    cartItems = cartItems.concat({
+      ...products.find(prod => prod.title === selectedTitle)
+    })
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  section {
+    width: 30rem;
+    margin: auto;
+  }
+
+  label,
+  input {
+    width: 100%;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<section>
+  <Cart items={cartItems} />
+</section>
+
+<hr />
+
+<section>
+  <div>
+    <label for="title">Title</label>
+    <input id="title" type="text" value={title} on:input={setTitle} />
+  </div>
+
+  <div>
+    <label for="price">Price</label>
+    <input id="price" type="number" bind:value={price} />
+  </div>
+
+  <div>
+    <label for="description">Description</label>
+    <textarea id="description" rows="3" bind:value={description} />
+  </div>
+
+  <Button on:click={createProduct}>Create Product</Button>
+</section>
+
+<section>
+  {#if products.length === 0}
+    <p>No products were added yet!</p>
+  {:else}
+    {#each products as product}
+      <Product
+        productPrice={product.price}
+        productTitle={product.title}
+        productDescription={product.description}
+        on:addtocart={addToCart} />
+    {/each}
+  {/if}
+</section>
---
+++ src/Cart.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let items;
+
+  $: cartTotal = items.reduce((sum, curValue) => {
+    return sum + curValue.price;
+  }, 0);
+</script>
+
+<style>
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 1rem;
+  }
+
+  li {
+    margin: 0.5rem 0;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    margin: 1rem;
+  }
+</style>
+
+{#if items.length === 0}
+  <p>No items in cart yet.</p>
+{:else}
+  <ul>
+    {#each items as item}
+      <li>{item.title} - {item.price}</li>
+    {/each}
+  </ul>
+{/if}
+
+<h1>Total: ${cartTotal}</h1>
